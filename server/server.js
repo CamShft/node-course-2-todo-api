946b950def107781ab32acb487a7bb0a99318108
Cote <samuel.cote@cgi.com>
2017-10-10 10:34:11 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
+var {ObjectID} =require('mongodb');
+
 app.use(bodyParser.json());
 
 app.post('/todos',(req, res)=>{
@@ -30,6 +32,24 @@ app.get('/todos',(req, res)=>{
   });
 });
 
+//GET /todos/1234324
+app.get('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+
+});
 app.listen(3000,()=>{
   console.log('Started on port 3000');
 });
